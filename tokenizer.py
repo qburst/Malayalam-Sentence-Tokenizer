1cf7e37c39f4920c08554d8a78aba49f31e9a470
Sabique AL <sabique@qburst.com>
2020-03-03 11:57:17 +0530

    Update tokenizer to work with quotes and question mark

--- tokenizer.py
+++ tokenizer.py
@@ -7,7 +7,9 @@ ABBREVIATION_PATH = './abbreviations.txt'
 
 class RuleBasedSentenceTokenizer():
     def __init__(self):
-        self.regex = r'([ഀ-ൿ\u200C\u200D"A-Za-z]+ *\.|[0-9]+ *\. )'
+        self.regex = r'([ഀ-ൿ\u200C\u200D"”?A-Za-z]+ *\.+|[0-9]+ *\. |[ഀ-ൿ\u200C\u200D"”A-Za-z]+ *\?)'
+        self.quote_regex = r'["”“]+'
+
         self.abb_list = self.__read_abbreviations(ABBREVIATION_PATH)
 
     def __read_abbreviations(self, model):
@@ -22,12 +24,18 @@ class RuleBasedSentenceTokenizer():
         for data in dataset:
             sentence_ends = re.findall(self.regex, data)
             for end in sentence_ends:
-                if end.lower() not in self.abb_list:
-                    end_idx = data.index(end)+len(end)
-                    sentence = data[: end_idx]
+                end_idx = data.index(end)+len(end)
+                sentence = data[: end_idx]
+                quotes_list = re.findall(self.quote_regex, sentence)
+                if (quotes_list and ((
+                    sentence.count('"') % 2 > 0) or (
+                        sentence.count('“') == sentence.count('”')))) or (
+                                not quotes_list and \
+                                        end.lower() not in self.abb_list):
                     sentences.append(sentence)
                     data = data[end_idx:]
             if data:
                 sentences.append(data)
         return sentences
+
 
